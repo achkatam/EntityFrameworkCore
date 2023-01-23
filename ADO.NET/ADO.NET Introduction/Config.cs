db85658c913c5aa019004587492218489983ec39
achkatam <agmatevv@gmail.com>
2023-01-23 15:44:13 -0500

    deleted config

--- ADO.NET/ADO.NET Introduction/Config.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ASP.NETConnectionToSQLServerInDocker
-{
-    public static class Config
-    {
-        //gitignore this file
-        public static string ConnectionString = @"Server=localhost\sqltest,1433;Database=SoftUni;User=achkatam;Password=yourStrong(!)Password";
-    }
-}
