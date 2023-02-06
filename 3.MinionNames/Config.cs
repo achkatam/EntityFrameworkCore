fcd18afef498c2040c69d906086ad345ac9aa77c
achkatam <agmatevv@gmail.com>
2023-02-06 09:20:44 -0500

    added EFC

---
+++ 3.MinionNames/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace _3.MinionNames
+{ 
+    public static class Config
+    {
+        public const string ConnectionString =
+                    @"Server=localhost\sqltest,1433;Database=MinionsDB;User=achkatam;Password=yourStrong(!)Password";
+    }
+}
---
+++ 4.AddMinion/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace _4.AddMinion
+{
+    
+    public static class Config
+    {
+        public const string ConnectionString
+                    = @"Server=localhost\sqltest,1433;Database=MinionsDB;User=achkatam;Password=yourStrong(!)Password";
+    }
+}
