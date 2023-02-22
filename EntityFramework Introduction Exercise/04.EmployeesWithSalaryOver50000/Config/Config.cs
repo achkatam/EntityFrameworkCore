2a559e8c0cb64a3eb0e021842f88da0c167f2b6e
achkatam <agmatevv@gmail.com>
2023-02-22 09:58:12 -0500

    deleted configuration file

--- EntityFramework Introduction Exercise/04.EmployeesWithSalaryOver50000/Config/Config.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SoftUni.Config
-{ 
-    public static class Config
-    {
-        public const string ConnectionString =
-            "Server=localhost\\sqltest,1433;Database=SoftUni;User=achkatam;Password=yourStrong(!)Password;TrustServerCertificate=True;";
-    }
-}
--- EntityFramework Introduction Exercise/14.DeletedProjectById/NewFolder/Config.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SoftUni.NewFolder
-{
-    public static class Config
-    {
-        public const string ConnectionString
-            = "Server=localhost\\sqltest,1433;Database=SoftUni;User=achkatam;Password=yourStrong(!)Password;TrustServerCertificate=True;";
-    }
-}
