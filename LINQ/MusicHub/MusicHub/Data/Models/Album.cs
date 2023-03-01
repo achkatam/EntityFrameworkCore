77037b0a2a49b208f6d52b8b5d45d07778e929d5
achkatam <agmatevv@gmail.com>
2023-03-01 13:11:42 -0500

    added solution from LINQ

---
+++ LINQ/MusicHub/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,29 @@
+﻿namespace MusicHub.Data.Models;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+using System.ComponentModel.DataAnnotations;
+public class Album
+{ 
+    public Album()
+    {
+        this.Songs = new HashSet<Song>();
+    }
+    [Key]
+    public int Id { get; set; }
+
+    [MaxLength(ValidationConstants.AlbumNameMaxLength)]
+    public string Name { get; set; } = null!;
+
+    [NotMapped] // excludes it in DB
+    public decimal Price
+        => this.Songs.Sum(s => s.Price);
+
+    public DateTime ReleaseDate { get; set; }
+
+    [ForeignKey(nameof(Producer))]
+    public int? ProducerId { get; set; }
+    public virtual Producer? Producer { get; set; }
+
+    public virtual ICollection<Song> Songs { get; set; }
+}
---
+++ LINQ/MusicHub/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+
+namespace MusicHub.Data;
+
+using Models;
+using Microsoft.EntityFrameworkCore;
+
+public class MusicHubDbContext : DbContext
+{
+    public MusicHubDbContext()
+    {
+    }
+
+    public MusicHubDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder
+                .UseSqlServer(Configuration.ConnectionString);
+        }
+    }
+
+    public DbSet<Song> Songs { get; set; }
+    public DbSet<Album> Albums { get; set; }
+
+    public DbSet<Producer> Producers { get; set; }
+
+    public DbSet<Performer> Performers { get; set; }
+
+    public DbSet<Writer> Writers { get; set; }
+
+    public DbSet<SongPerformer> SongsPerformers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Song>(e =>
+        {
+            e
+                .Property(s => s.CreatedOn)
+                .HasColumnType("date");
+        });
+
+        builder.Entity<Album>(a =>
+        {
+            a
+                .Property(a => a.ReleaseDate)
+                .HasColumnType("date");
+        });
+
+        builder.Entity<SongPerformer>(sp =>
+        {
+            sp.HasKey(k => new { k.SongId, k.PerformerId });
+        });
+    }
+}
+
