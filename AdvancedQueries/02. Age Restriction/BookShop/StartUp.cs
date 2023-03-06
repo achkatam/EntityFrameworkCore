a46b90ccf4346cf79b6bafcf9f510e2b7dd1304f
achkatam <agmatevv@gmail.com>
2023-03-06 12:23:58 -0500

    added solution for advanced queries

---
+++ AdvancedQueries/02. Age Restriction/BookShop/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace BookShop;
+
+using Data;
+using Initializer;
+using Models.Enums;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var db = new BookShopContext();
+        // DbInitializer.ResetDatabase(db); 
+
+        string ageRestrictionInput = Console.ReadLine();
+
+        string result = GetBooksByAgeRestriction(db, ageRestrictionInput);
+
+        Console.WriteLine(result);
+    }
+
+    public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+    {
+        bool hasParsed = Enum.TryParse(typeof(AgeRestriction), command, true, out object ageRestrictionObj);
+
+        AgeRestriction ageRestriction;
+
+        if (hasParsed)
+        {
+            ageRestriction = (AgeRestriction)ageRestrictionObj;
+
+
+            string[] books = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+        return null;
+    }
+}
