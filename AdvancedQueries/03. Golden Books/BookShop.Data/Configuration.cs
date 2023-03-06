ec5f3847ce6c6aeb37994e7b68fa210c3344fe23
achkatam <agmatevv@gmail.com>
2023-03-06 12:36:00 -0500

    added exercise

---
+++ AdvancedQueries/03. Golden Books/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=localhost\\sqltest,1433;Database=BookShop;User=achkatam;Password=yourStrong(!)Password;";
+    }
+}
---
+++ AdvancedQueries/03. Golden Books/BookShop/StartUp.cs
@@ -0,0 +1,61 @@
+﻿namespace BookShop;
+
+using Data;
+using Initializer;
+using Models.Enums;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var db = new BookShopContext();
+        // DbInitializer.ResetDatabase(db); 
+
+        // 02
+        //  string ageRestrictionInput = Console.ReadLine();
+        // string result = GetBooksByAgeRestriction(db, ageRestrictionInput);
+
+        // 03
+        string result = GetGoldenBooks(db);
+
+        Console.WriteLine(result);
+    }
+
+    // 02
+    public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+    {
+        bool hasParsed = Enum.TryParse(typeof(AgeRestriction), command, true, out object ageRestrictionObj);
+
+        AgeRestriction ageRestriction;
+
+        if (hasParsed)
+        {
+            ageRestriction = (AgeRestriction)ageRestrictionObj;
+
+
+            string[] books = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+        return null;
+    }
+
+    // 03
+    public static string GetGoldenBooks(BookShopContext dbContext)
+    {
+        string[] books = dbContext.Books
+            .Where(b => b.EditionType == EditionType.Gold &&
+                        b.Copies < 5000)
+            .OrderBy(b => b.BookId)
+            .Select(b => b.Title)
+            .ToArray();
+
+
+        return string.Join(Environment.NewLine, books);
+    }
+}
