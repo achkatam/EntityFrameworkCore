95012e58d7cbe700027c27978304170e63eac9af
achkatam <agmatevv@gmail.com>
2023-03-10 12:18:47 -0500

    created JSON exersies and added one solution

---
+++ JavaScript Object Notation - JSON Processing/01.ImportUsers/DTOs/Import/User/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace ProductShop.DTOs.Import.User
+{
+    using Newtonsoft.Json;
+
+    [JsonObject]
+    public class UserDTO
+    {
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("age")]
+        public int Age { get; set; }
+    }
+}
---
+++ JavaScript Object Notation - JSON Processing/01.ImportUsers/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace ProductShop
+{
+    using Newtonsoft.Json;
+    using Models;
+
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext dbContext = new ProductShopContext();
+
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText(@"../../../Datasets/users.json");
+            string result = ImportUsers(dbContext, inputJson);
+
+            Console.WriteLine(result);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            ICollection<User> users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+    }
+}
