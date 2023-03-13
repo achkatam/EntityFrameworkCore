44e0b210550b666d3d197cb1aa72c41fadeed0c4
achkatam <agmatevv@gmail.com>
2023-03-13 15:10:16 -0400

    added solution

---
+++ JavaScript Object Notation - JSON Processing/03.ImportCategories/ProductShopProfile.cs
@@ -0,0 +1,17 @@
+﻿namespace ProductShop;
+
+using DTOs.Import.Category;
+using DTOs.Import.Product;
+using DTOs.Import.User;
+using Models;
+using AutoMapper;
+
+public class ProductShopProfile : Profile
+{
+    public ProductShopProfile()
+    {
+        this.CreateMap<ImportUserDto, User>();
+        this.CreateMap<ImportProductDto, Product>();
+        this.CreateMap<ImportCategoryDto, Category>();
+    }
+}
