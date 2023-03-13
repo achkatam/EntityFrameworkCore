550566f2b8f6a0ce5480e9240640f46932006cae
achkatam <agmatevv@gmail.com>
2023-03-13 16:20:19 -0400

    added solution

---
+++ JavaScript Object Notation - JSON Processing/06.ExportSoldProducts/DTOs/Import/Category/ImportCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductShop.DTOs.Import.Category;
+
+using Newtonsoft.Json;
+
+[JsonObject]
+public class ImportCategoryDto
+{
+    [JsonProperty("name")]
+    public string? Name { get; set; }
+
+}
