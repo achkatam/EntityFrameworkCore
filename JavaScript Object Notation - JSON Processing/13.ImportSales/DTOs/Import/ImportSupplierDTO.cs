461e617c190f7f0d2ae55993b95e2acc2401aef5
achkatam <agmatevv@gmail.com>
2023-03-14 12:34:23 -0400

    added solution

---
+++ JavaScript Object Notation - JSON Processing/13.ImportSales/DTOs/Import/ImportSupplierDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace CarDealer.DTOs.Import;
+
+using Newtonsoft.Json;
+
+[JsonObject]
+public class ImportSupplierDTO
+{
+    [JsonProperty("name")]
+    public string Name { get; set; } = null!;
+
+    [JsonProperty("isImporter")]
+    public bool IsImporter { get; set; }
+}
---
+++ JavaScript Object Notation - JSON Processing/13.ImportSales/Models/Sale.cs
@@ -0,0 +1,13 @@
+﻿namespace CarDealer.Models;
+public class Sale
+{
+    public int Id { get; set; }
+
+    public decimal Discount { get; set; }
+
+    public int CarId { get; set; }
+    public virtual Car Car { get; set; } = null!;
+
+    public int CustomerId { get; set; }
+    public virtual Customer Customer { get; set; } = null!;
+}
