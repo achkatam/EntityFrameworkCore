7fdeb72d4b609307ea53b082abc42b90632479d5
achkatam <agmatevv@gmail.com>
2023-03-14 12:47:33 -0400

    added solution

---
+++ JavaScript Object Notation - JSON Processing/14.ExportOrderedCustomers/CarDealerProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace CarDealer;
+
+using DTOs.Import;
+using Models;
+using AutoMapper;
+
+public class CarDealerProfile : Profile
+{
+    public CarDealerProfile()
+    {
+        this.CreateMap<ImportSupplierDTO, Supplier>();
+
+        this.CreateMap<ImportPartDto, Part>();
+    }
+}
