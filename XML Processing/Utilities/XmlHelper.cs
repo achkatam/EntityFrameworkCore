e4a2573add2a33e5c79ad6fb084f3d9303f0433c
achkatam <agmatevv@gmail.com>
2023-03-18 09:37:11 -0400

    added solution for 9th exercise

---
+++ XML Processing/Utilities/XmlHelper.cs
@@ -0,0 +1,38 @@
+﻿namespace CarDealer.Utilities;
+
+using System.Xml.Serialization;
+
+
+
+public   class XmlHelper
+{
+    public T Deserialize<T>(string inputXml, string rootName)
+    {
+        XmlRootAttribute xmlRoot = new XmlRootAttribute("Suppliers");
+
+
+        XmlSerializer xmlSerializer =
+            new XmlSerializer(typeof(T), xmlRoot);
+
+        StringReader reader = new StringReader(inputXml);
+
+        T suppliersDtos = (T)xmlSerializer.Deserialize(reader);
+
+        return suppliersDtos;
+    }
+
+    public IEnumerable<T> DeserializeCollection<T>(string inputXml, string rootName)
+    {
+        XmlRootAttribute xmlRoot = new XmlRootAttribute("Suppliers");
+
+
+        XmlSerializer xmlSerializer =
+            new XmlSerializer(typeof(T[]), xmlRoot);
+
+        StringReader reader = new StringReader(inputXml);
+
+        T[] suppliersDtos = (T[])xmlSerializer.Deserialize(reader);
+
+        return suppliersDtos;
+    }
+}
