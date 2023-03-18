e47dd6d851828a531bc153fadf510e541221cdb4
achkatam <agmatevv@gmail.com>
2023-03-18 11:56:39 -0400

    initial commit

---
+++ XML Processing/ProductStore/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace ProductShop;
+
+using DTOs.Import;
+using Models;
+using Utilities;
+using AutoMapper;
+using Data;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        ProductShopContext dbContext = new ProductShopContext();
+
+        string inputXml = File.ReadAllText("../../../Datasets/users.xml");
+        string result = ImportUsers(dbContext, inputXml);
+
+        Console.WriteLine(result);
+    }
+
+    public static string ImportUsers(ProductShopContext dbContext, string inputXml)
+    {
+        IMapper mapper = CreateMapper();
+        XmlHelper xmlHelper = new XmlHelper();
+
+        ImportUserDto[] userDtos = xmlHelper.Deserializer<ImportUserDto[]>(inputXml, "Users");
+
+        ICollection<User> users = new HashSet<User>();
+
+        foreach (var userDto in userDtos)
+        {
+            User user = mapper.Map<User>(userDto);
+
+            users.Add(user);
+        }
+
+        dbContext.AddRange(users);
+        // dbContext.SaveChanges();
+
+        return $"Successfully imported {users.Count}";
+    }
+
+    private static IMapper CreateMapper()
+        => new Mapper(new MapperConfiguration(cfg =>
+        {
+
+            cfg.AddProfile<ProductShopProfile>();
+        }));
+}
---
+++ XML Processing/ProductStore/Utilities/XmlHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace ProductShop.Utilities;
+
+using System.Xml.Serialization;
+public class XmlHelper
+{
+    public  T Deserializer<T>(string inputXml, string rootName)
+    {
+        XmlRootAttribute root = new XmlRootAttribute(rootName);
+        XmlSerializer serializer = new XmlSerializer(typeof(T), root);
+
+        using StringReader reader = new StringReader(inputXml);
+
+        T dtos = (T)serializer.Deserialize(reader);
+
+        return dtos;
+    } 
+}
