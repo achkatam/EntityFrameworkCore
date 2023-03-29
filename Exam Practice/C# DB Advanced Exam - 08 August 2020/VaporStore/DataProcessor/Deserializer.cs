9a5e7d19c87ffa0d1be3ef3005452dba52849b08
achkatam <agmatevv@gmail.com>
2023-03-29 08:57:06 -0400

    Imported data for games

--- Exam Practice/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Deserializer.cs
+++ Exam Practice/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Deserializer.cs
@@ -1,8 +1,13 @@
 ﻿namespace VaporStore.DataProcessor
 {
     using System.ComponentModel.DataAnnotations;
-
+    using System.Globalization;
+    using System.Text;
+    using System.Xml;
     using Data;
+    using Data.Models;
+    using ImportDto;
+    using Newtonsoft.Json;
 
     public static class Deserializer
     {
@@ -16,7 +21,81 @@
 
         public static string ImportGames(VaporStoreDbContext context, string jsonString)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder();
+            var gameDtos = JsonConvert.DeserializeObject<GameImportDto[]>(jsonString);
+
+            var games = new HashSet<Game>();
+            var developers = new HashSet<Developer>();
+            var genres = new HashSet<Genre>();
+            var tags = new HashSet<Tag>();
+
+            foreach (var dto in gameDtos)
+            {
+                bool releaseDateCheck = DateTime.TryParseExact(dto.ReleaseDate, "yyyy-MM-dd",
+                    CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime releaseDate);
+
+                if (!IsValid(dto)
+                    || dto.Price < 0
+                    || string.IsNullOrWhiteSpace(dto.Name)
+                    || !releaseDateCheck
+                    || string.IsNullOrWhiteSpace(dto.Genre)
+                    || string.IsNullOrWhiteSpace(dto.Developer)
+                    || !dto.Tags.Any())
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Game game = new Game()
+                {
+                    Name = dto.Name,
+                    Price = dto.Price,
+                    ReleaseDate = releaseDate
+                };
+
+                Developer dev = developers.FirstOrDefault(d => d.Name == dto.Developer);
+                if (dev is null)
+                {
+                    dev = new Developer() { Name = dto.Developer };
+                    developers.Add(dev);
+                }
+                game.Developer = dev;
+
+                Genre genre = genres.FirstOrDefault(x => x.Name == dto.Genre);
+                if (genre is null)
+                {
+                    genre = new Genre()
+                    {
+                        Name = dto.Genre
+                    };
+                    genres.Add(genre);
+                }
+                game.Genre = genre;
+
+                foreach (string tagDto in dto.Tags)
+                {
+                    Tag tag = tags.FirstOrDefault(x => x.Name == tagDto);
+                    if (tag is null)
+                    {
+                        tag = new Tag() { Name = tagDto };
+                        tags.Add(tag);
+                    }
+
+                    GameTag gameTag = new GameTag()
+                    {
+                        Game = game,
+                        Tag = tag
+                    };
+
+                    game.GameTags.Add(gameTag);
+                }
+                games.Add(game);
+                sb.AppendLine(string.Format(SuccessfullyImportedGame, game.Name, genre.Name ,game.GameTags.Count));
+            }
+            context.Games.AddRange(games);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string ImportUsers(VaporStoreDbContext context, string jsonString)
---
+++ Exam Practice/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/ImportDto/GameImportDto.cs
@@ -0,0 +1,24 @@
+﻿namespace VaporStore.DataProcessor.ImportDto
+{ 
+    using Newtonsoft.Json;
+
+    public class GameImportDto
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; } = null!;
+
+        [JsonProperty("Price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty("ReleaseDate")]
+        public string ReleaseDate { get; set; } = null!;
+        [JsonProperty("Developer")]
+        public string Developer { get; set; } = null!;
+
+        [JsonProperty("Genre")]
+        public string Genre { get; set; } = null!;
+
+        [JsonProperty("Tags")]
+        public string[] Tags { get; set; } = null!;
+    } 
+}
